94a884f51c8ec3294bd30de54821ad33ba84a404
RidaNaz <rnaz3414@gmail.com>
2024-11-30 21:12:00 +0500

    Commit by Rida

--- app.py
+++ app.py
@@ -269,6 +269,32 @@ graph = builder.compile(checkpointer=checkpointer, interrupt_before=["human"])
 
 ####
 
+@cl.set_starters
+async def set_starters():
+    return [
+        cl.Starter(
+            label="Text inviting friend to wedding",
+            message="Write a text asking a friend to be my plus-one at a wedding next month. I want to keep it super short and casual, and offer an out.",
+            icon="/public/favicon.png",
+            ),
+        cl.Starter(
+            label="Morning routine ideation",
+            message="Can you help me create a personalized morning routine that would help increase my productivity throughout the day? Start by asking me about my current habits and what activities energize me in the morning.",
+            icon="/public/idea.png",
+            ),
+
+        cl.Starter(
+            label="Explain superconductors",
+            message="Explain superconductors like I'm five years old.",
+            icon="/public/search.png",
+            ),
+        cl.Starter(
+            label="Python script for daily email reports",
+            message="Write a script to automate sending daily email reports in Python, and walk me through how I would set it up.",
+            icon="/public/pen.png",
+            )
+        ]
+
 @cl.on_message
 async def on_message(msg: cl.Message):
     config = {"configurable": {"thread_id": cl.context.session.id}}
